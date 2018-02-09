4846e152ceeea80e6f43739f6a140406c0012c25
Tarun Gupta <12tarungupta@gmail.com>
2018-02-09 20:31:44 +0530

    Initial Commit

---
+++ Notestash/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using Notestash.Models;
+
+namespace Notestash.Controllers
+{
+    public class UserController : ApiController
+    {
+        [HttpPost]
+        public HttpResponseMessage Create(UserModel objUser)
+        {
+            if (!ModelState.IsValid)
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Model State Invalid");
+
+            UserModel ob = new UserModel();
+            bool created = ob.Create(objUser);
+
+            if (created)
+                return Request.CreateResponse(HttpStatusCode.Created, "Successfully Registered");
+            return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Registration Unsuccessful");
+        }
+    }
+}
---
+++ Notestash/Models/UserModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using NotestashDataAccess;
+
+namespace Notestash.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        [Required] public string FullName { get; set; }
+        [Required] public string Password { get; set; }
+
+        [Required]
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$")]
+        public string Email { get; set; }
+
+        public bool Create(UserModel objUser)
+        {
+            try
+            {
+                tblUser objTblUser = new tblUser();
+                objTblUser.Id = objUser.Id;
+                objTblUser.FullName = objUser.FullName;
+                objTblUser.Password = objUser.Email;
+                objTblUser.Email = objUser.Password;
+
+                using (NoteStashDBEntities db = new NoteStashDBEntities())
+                {
+                    db.tblUsers.Add(objTblUser);
+                    db.SaveChanges();
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
